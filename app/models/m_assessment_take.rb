e11b53350604e0dd33a025a56f6f2917e1a21f2c
pfc <passionateforchrist@gmail.com>
2010-06-04 00:55:56 -0400

    update models + started work on assessment view

--- app/models/m_assessment_take.rb
+++ app/models/m_assessment_take.rb
@@ -1,7 +1,6 @@
 class MAssessmentTake < ActiveRecord::Base
+  has_many :m_question_takes, :dependent => :destroy
   belongs_to :m_assessment
   belongs_to :user
-  
-  has_many :m_question_takes, :dependent => :destroy
   validates_presence_of :m_assessment_id, :user_id
 end
--- app/models/m_question.rb
+++ app/models/m_question.rb
@@ -1,5 +1,6 @@
 class MQuestion < ActiveRecord::Base
   belongs_to :m_assessment
-  validates_presence_of :body, :m_assessment_id
-  has_many :m_question, :dependent => :destroy
+  validates_presence_of :body
+  validates_presence_of :m_assessment_id
+  has_many :m_answers, :dependent => :destroy
 end
